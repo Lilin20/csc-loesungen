d1cc8e18621f096a85f4187837db85925b54ec81
Lilin20 <marvingrunwald2000@gmail.com>
2022-08-11 12:34:31 +0200

    First upload

---
+++ 0: Hallo, Welt/1Bonjour.py
@@ -0,0 +1,8 @@
+# Lösung zur Aufgabe "Bonjour".
+# Hierbei soll ein einfacher Satz mit der Hilfe der "Print"-Funktion ausgegeben werden
+
+print("Bonjour, tout le monde!")
+
+# Wichtig hierbei ist, den Satz in anführungszeichen zu setzen.
+# Ohne die Anführungszeichen, interpretiert Python das komplett anders.
+# Python verscht z.B. das Bonjour als Variable zu interpretieren aber diese Variable ist nicht von uns gesetzt worden.
