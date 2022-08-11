b1ebdd98df7b3de7d98815b9609d8f2373557e40
Lilin20 <marvingrunwald2000@gmail.com>
2022-08-11 12:41:40 +0200

    Upload

---
+++ 1: Variablen/x Marks the Spot.py
@@ -0,0 +1,9 @@
+# Die Aufgabe besteht darin den Wert für x zu berechnen.
+# Die Aufgabe verlangt das man die Aufgabe im Kopf berechnet.
+# Die Variable x wird in jeder Zeile neu bestimmt.
+
+x = 10		# x = 10
+x = x + x	# x = 20
+x = x - 5	# x = 15
+
+# Der Wert für x Beträgt: 15.
